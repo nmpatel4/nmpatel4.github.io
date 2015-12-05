0648f606b06ce52e25b8cfb6e71715eac141512f
Nirav Patel <nirav-patel@kellogg.northwestern.edu>
2015-12-04 19:49:02 -0600

    Final Project, commit 4
    Final Project, commit 4

---
+++ KelloggCoursesToTake.js
@@ -0,0 +1,8 @@
+// This code runs when the page loads
+$(function() {
+  $(".course").on("click", function(event) {
+    event.preventDefault();
+    var elementThatWasClicked = $(this);
+    elementThatWasClicked.remove();
+  })
+})
